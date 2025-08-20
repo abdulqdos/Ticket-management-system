069e56620e6b1045348dc8540882c4c65fb74291
abdulqdos <abdulqdosalabinie@gmail.com>
2025-08-20 15:47:35 +0200

    Add Traslate For Tables

--- app/Enum/EventState.php
+++ app/Enum/EventState.php
@@ -7,5 +7,22 @@ enum EventState: string
     case PENDING = "PENDING";
     case ACTIVE = "ACTIVE";
     case INACTIVE = "INACTIVE";
-    case CANCELLED = "CANCELLED";
+
+    public function getColor(): string
+    {
+        return match ($this) {
+            self::PENDING => 'warning',
+            self::ACTIVE => 'success',
+            self::INACTIVE => 'danger',
+        };
+    }
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::PENDING => __('PENDING'),
+            self::ACTIVE => __('ACTIVE'),
+            self::INACTIVE => __('INACTIVE'),
+        };
+    }
 }
--- app/Filament/Resources/Events/Tables/EventsTable.php
+++ app/Filament/Resources/Events/Tables/EventsTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Events\Tables;
 
+use App\Enum\EventState;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
@@ -22,8 +23,9 @@ class EventsTable
                     ->searchable(),
                 TextColumn::make('state')
                     ->label(__('State'))
-                    ->badge('primary')
-                    ->searchable(),
+                    ->badge()
+                    ->formatStateUsing(fn (string $state) => EventState::from($state)->getLabel())
+                    ->color(fn (string $state) => EventState::from($state)->getColor()),
                 TextColumn::make('date')
                     ->label(__('Date'))
                     ->date('d-m-Y')
