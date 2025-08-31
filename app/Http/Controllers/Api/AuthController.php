20162171abfa2ab7d1bda5bd7ea63a4561dadad1
abdulqdos <abdulqdosalabinie@gmail.com>
2025-08-31 14:00:05 +0200

    End Logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginRequest;
+use App\Models\Customer;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use App\Traits\Api\ApiResponses;
 class AuthController extends Controller
 {
 
+    use ApiResponses ;
     public function login(LoginRequest $request)
     {
         // Validate Request
@@ -17,19 +20,33 @@ class AuthController extends Controller
 
         // Make sure The User Have account
         if (!Auth::guard('customer')->attempt($request->only('phone', 'password'))) {
-            return $this->error('Invalid credentials', 401);
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
         }
 
+        // Get Customer
+        $customer = Customer::firstWhere('phone', $request->phone);
+
         // Return Response
-        return  response()->json([
-          "message" =>  $request->all(),
-        ]);
+        return $this->ok(
+            'Authenticated',
+            [
+                'token' => $customer->createToken('API token for ' . $customer->phone)->plainTextToken,
+            ]
+        );
     }
 
 
-    public function logout(Request $request)
+    public function logout(Request $request): JsonResponse
     {
-        return "we are good";
+        $customer = $request->user('customer');
+
+        if ($customer) {
+            $customer->currentAccessToken()->delete();
+        }
+        
+        return $this->ok('Logged out successfully');
     }
 
     public function Register(Request $request)
--- tests/Feature/Auth/loginTest.php
+++ tests/Feature/Auth/loginTest.php
@@ -1,3 +1,36 @@
 <?php
 
-it('Hello Test' , fn() => $this->assertTrue(true));
+use App\Models\Customer;
+use App\Models\User;
+use function Pest\Laravel\post;
+use Illuminate\Support\Facades\Hash;
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+//    dd($this->user);
+});
+
+
+
+beforeEach(function () {
+    $this->user = Customer::factory()->create([
+        'phone' => '0916050469',
+        'password' => Hash::make('password123'),
+    ]);
+});
+
+it('Login with correct credentials returns 200', function () {
+    post('/api/auth/login', [
+        'phone' => $this->user->phone,
+        'password' => "password123",
+    ])->assertStatus(200);
+});
+
+it('Login with wrong credentials returns 401', function () {
+    post('/api/auth/login', [
+        'phone' => $this->user->phone,
+        'password' => 'wrongpassword',
+    ])->assertStatus(401);
+});
+
+
